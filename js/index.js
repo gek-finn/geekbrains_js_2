54dd5082ae663172d98c4508356582a10e5a061c
gek-finn <55052551+gek-finn@users.noreply.github.com>
2019-12-09 02:04:34 +0200

    Домашняя работа ко второму уроку (не все сделано - не совсем разобрался с классами)

--- js/index.js
+++ js/index.js
@@ -1,17 +1,50 @@
-const goods = [
-    { title: 'Shirt', price: 150 + ' $' },
-    { title: 'Socks', price: 150 + ' $' },
-    { title: 'Jacket', price: 150 + ' $'  },
-    { title: 'Shoes', price: 150 + ' $' },
-];
+document.querySelector('.cart-button').addEventListener('click', () => {document.querySelector('.cart-block').classList.toggle('invisible');});
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><img src="img/shirt.jpg" alt="shirt img"><div class="desc"><h3>${title}</h3><p>${price}</p><button class="buy-btn">Добавить</button></div></div>`
-};
+class GoodsItem {
+    constructor(title = 'Без имени', price = '150') {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><img src="img/shirt.jpg" alt="shirt img"><div class="desc"><h3>${this.title}</h3><p>${this.price} ₽</p><button class="buy-btn">Добавить</button></div></div>`;
+    }
+}
 
-const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-};
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods()  {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 150 },
+            { title: 'Jacket', price: 150 },
+            { title: 'Shoes', price: 150 },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+    
+    sumGoods() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        })
+        return sum;
+        
+    }
+}
+class UserCart {
+    
+}
 
-renderGoodsList(goods);
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
